a7b37d957f23147494789c04d8fbc1b2276b6333
Pablo Garcia <pgarcia@dragoons.io>
2019-01-03 14:03:36 -0300

    Eject Rollback

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        {/* Sentry Integration */}
-          <script src="https://cdn.ravenjs.com/3.26.2/raven.min.js" crossOrigin="anonymous"></script>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Dragoons X with React</h1>
+          <h1 className="App-title">Welcome to Jenkins X with React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/components/App.js</code> and save to reload.
